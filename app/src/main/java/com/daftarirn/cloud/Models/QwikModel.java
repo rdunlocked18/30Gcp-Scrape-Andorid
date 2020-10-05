4bd3c8de5346096c92c655bcfae8c6060ebae278
rdunlocked <rohitdaftari3@gmail.com>
2020-10-06 00:38:56 +0530

    initial

---
+++ app/src/main/java/com/daftarirn/cloud/Models/QwikModel.java
@@ -0,0 +1,16 @@
+package com.daftarirn.cloud.Models;
+
+public class QwikModel {
+
+    String name;
+    String rank;
+    String qwiklabs_id;
+    String id;
+    int quests_status;
+    String dpUrl;
+    String questsNames;
+
+
+
+
+}
